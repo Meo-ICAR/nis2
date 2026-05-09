1e45cbc8025b3e58402181e697bd608b2b5de8fa
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-05-09 10:35:28 +0200

    ok si collega

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -28,5 +30,19 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected static function booted(): void
+    {
+        static::creating(function (User $user) {
+            // Se il nome è vuoto o null, assegnagli il valore dell'email
+            if (empty($user->name)) {
+                $user->name = $user->email;
+            }
+            // 2. Se la password è vuota (es. login via SSO), genera una password casuale sicura
+            if (empty($user->password)) {
+                $user->password = Hash::make(Str::random(32));
+            }
+        });
     }
 }
